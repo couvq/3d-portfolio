296a2f067c2cea7d849fa51545613102f196b4ee
couvq <quentincouvelaire@gmail.com>
2023-11-10 19:38:01 -0600

    setup basic routing with empty pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 const App = () => {
   return (
     <main className="bg-slate-300/20">
       <Router>
-        Navbar
+        <Navbar />
         <Routes>
-          <Route path="/" element={'Home'}/>
-          <Route path="/about" element={'About'} />
-          <Route path="/projects" element={'Projects'} />
-          <Route path="/contact" element={'Contact'} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
